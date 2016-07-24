809280c57d7dbaf08a3b94dc1b665c3ceaa13ea6
milenstanev <mstanev@softserveinc.com>
2016-07-25 02:41:58 +0300

    Added core directives

---
+++ default/directives/directives.js
@@ -0,0 +1,31 @@
+import AngularModuleHelper from '../AngularModuleHelper.js';
+
+
+class CoreDirectives extends AngularModuleHelper {
+	constructor(moduleName, moduleDependencies) {
+		super(moduleName, moduleDependencies);
+
+		/**
+		 * Widgets Placeholder - loading directives
+		 */
+		this.directive('mswWidgetPlaceholder', [
+			'$compile',
+			($compile) => {
+				return {
+					scope: {
+						directiveName: '='
+					},
+					link: (scope, element) => {
+						element.append(`<div ${scope.directiveName}></div>`);
+						$compile(element.contents())(scope);
+					}
+				}
+			}
+		]);
+
+	}
+}
+
+const mswCoreDirectives = new CoreDirectives();
+
+export default mswCoreDirectives;
